6aa039b70ea710aa709a7d11bac4ecf8301eb52f
david <david@appsaloon.be>
2019-07-08 15:11:59 +0200

    basic useState example

--- src/index.js
+++ src/index.js
@@ -1,8 +1,8 @@
 import React from 'react'
 import ReactDOM from 'react-dom'
-import App from './App'
+import ClickCounter from './components/ClickCounter'
 
 ReactDOM.render(
-  <App/>,
+  <ClickCounter defaultValue={5}/>,
   document.getElementById('root')
 )
