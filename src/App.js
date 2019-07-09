2500be7b140c70390dac85217f57c9f767367553
david <david@appsaloon.be>
2019-07-09 17:19:22 +0200

    cleanup

--- src/App.js
+++ src/App.js
@@ -1,36 +1,11 @@
 import React, { Component } from 'react'
-import Display from './components/Display/Display'
-import Controls from './components/Controls/Controls'
 
 class App extends Component {
-
-  constructor (props) {
-    super(props)
-    this.state = {
-      isDisplayVisible: false,
-    }
-  }
-
-  toggleDisplayVisible = () => {
-    this.setState((state) => ({
-      isDisplayVisible: !state.isDisplayVisible,
-    }))
-  }
 
   render() {
     return (
       <div className="App">
 
-        <div>
-          <button onClick={this.toggleDisplayVisible}>toggle display on/off</button>
-        </div>
-
-        {
-          Boolean(this.state.isDisplayVisible) && (
-            <Display/>
-          )
-        }
-        <Controls/>
       </div>
     )
   }
--- src/redux/appState/reducer.js
+++ src/redux/appState/reducer.js
@@ -1,25 +1,9 @@
 const defaultState = {
-  counter: 0
 }
 
 export default (state = defaultState, action) => {
   const newState = Object.assign({}, state)
   switch (action.type) {
-
-    case 'INCREASE': {
-      newState.counter = state.counter + 1
-      return newState
-    }
-
-    case 'DECREASE': {
-      newState.counter = state.counter - 1
-      return newState
-    }
-
-    case 'RESET': {
-      newState.counter = 0
-      return newState
-    }
 
     default: {
       return state
