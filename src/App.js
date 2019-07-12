cf3552ac9445582856bbef9282567a21ff2d18b3
david <david@appsaloon.be>
2019-07-12 11:33:30 +0200

    simple controlled components example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,61 @@
 import React from 'react'
 
-function App () {
-  return (
-    <div className="App">
+class App extends React.Component {
 
-    </div>
-  )
+  constructor (props) {
+    super(props)
+    this.state = {
+      name: '',
+      comment: '',
+    }
+  }
+
+  handleChange = (event) => {
+    const {
+      name,
+      value,
+    } = event.target
+    this.setState((state) => ({
+      [name]: value,
+    }))
+  }
+
+  render () {
+    const {
+      name,
+      comment,
+    } = this.state
+
+    const formIsComplete = Boolean(name.length && comment.length)
+
+    return (
+      <div className="App">
+        <div>
+          <input
+            type="text"
+            name='name'
+            value={name}
+            onChange={this.handleChange}
+            placeholder='your name'
+          />
+        </div>
+
+        <div>
+          <textarea
+            name='comment'
+            value={comment}
+            onChange={this.handleChange}
+            placeholder='your comment'
+          />
+        </div>
+
+        <div>
+          <button disabled={!formIsComplete}>submit your comment</button>
+        </div>
+
+      </div>
+    )
+  }
 }
 
 export default App
