e6d6fbccfbfa3d4c53573e9a1c5923862435d663
david <david@appsaloon.be>
2019-07-12 11:01:14 +0200

    simple ref example

--- src/App.js
+++ src/App.js
@@ -1,11 +1,48 @@
 import React from 'react'
 
-function App () {
-  return (
-    <div className="App">
+class App extends React.Component {
 
-    </div>
-  )
+  constructor (props) {
+    super(props)
+    this.state = {
+      file: null,
+    }
+    this.filePickerRef = React.createRef()
+  }
+
+  handleClick = () => {
+    this.filePickerRef.current.click()
+  }
+  
+  handleChange = () => {
+    const files = Array.from(this.filePickerRef.current.files)
+    console.log('files: ', files)
+    if(files && files[0]) {
+      this.setState({
+        file: files[0]
+      })
+    }
+  }
+
+  render () {
+    return (
+      <div className="App">
+        <input
+          ref={this.filePickerRef}
+          type="file"
+          style={{display: 'none'}}
+          onChange={this.handleChange}
+        />
+
+        <button onClick={this.handleClick}>Pick a file</button>
+        {
+          Boolean(this.state.file) && (
+            <div>{this.state.file.name}</div>
+          )
+        }
+      </div>
+    )
+  }
 }
 
 export default App
